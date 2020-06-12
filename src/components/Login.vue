100f3d1490904e6e3041bc1ca96b880c07d71112
along <1156946971@qq.com>
2020-06-12 16:59:53 +0800

    完成了登录功能

---
+++ src/components/Login.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="头像" />
+      </div>
+      <!-- 登陆表单区域 -->
+      <el-form
+        label-width="0"
+        :model="loginForm"
+        :rules="loginFormRules"
+        ref="loginFormRef"
+        class="login_form"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input
+            placeholder="请输入用户名"
+            v-model="loginForm.username"
+            prefix-icon="iconfont icon-user"
+          ></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            placeholder="请输入密码"
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+            show-password
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登陆</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 登陆表单的数据绑定对象
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      // 表单验证对象
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入登录名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入登录密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    resetLoginForm() {
+      this.$refs.loginFormRef.resetFields()
+    },
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return false
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) {
+          return this.$message({
+            message: '用户名或密码不正确！',
+            type: 'error',
+            center: true
+          })
+        }
+        this.$message({ message: '登录成功！', type: 'success', center: true })
+        // 1.将登陆成功之后的token保存到客户端的sessionStorage中
+        //  1.1项目中除了登录之外的其他API接口，必须在登录之后才能访问
+        //  1.2token 只应在当前网站打开期间生效，所以将token保存到sessionStorage中
+        window.sessionStorage.setItem('token', res.data.token)
+        // 编程式路由跳转后台主页
+        this.$router.push('/home')
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  height: 100%;
+  background-color: #2b4b6b;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/', redirect: './login' },
+  { path: '/login', component: Login },
+  { path: '/home', component: Home }
 ]
-
 const router = new VueRouter({
   routes
+})
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  /* 1.to表示将要访问的路径
+      2.from表示从哪个路径跳转而来
+      3.next 是一个函数，表示放行
+      next() 放行 next('/login') 强制跳转 */
+  // 获取token 如果访问路径不是登录页面并且token值为空则强制跳转到登录界面
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (to.path !== '/login' && !tokenStr) next('/login')
+  // 加else 避免验证身份失败 执行强制跳转next()之后再执行下一个next()
+  else next()
 })
 
 export default router
