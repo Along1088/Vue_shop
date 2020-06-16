72e73cefd270274a8ef555eb0f63b0e0db126f1d
along <1156946971@qq.com>
2020-06-16 21:30:10 +0800

    完成用户列表的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,156 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409eff"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          router
+          :default-active="activePath"
+        >
+          <!-- 展开或收起按钮 -->
+          <div class="toogle-button" @click="toogleCollapse">|||</div>
+          <!-- 一级导航菜单 -->
+          <el-submenu
+            :index="item.id + ''"
+            v-for="item in menuList"
+            :key="item.id"
+          >
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconList[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{ item.authName }}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+              @click="saveNavState('/' + subItem.path)"
+            >
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{ subItem.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容区域 -->
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  created() {
+    this.getMenuList()
+    /* 给activePath赋值显示到界面中，刷新也可以高亮之前点击的菜单 */
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
+  data() {
+    return {
+      /* 左侧菜单数据 */
+      menuList: [],
+      /* 一级菜单对应的图标 */
+      iconList: {
+        125: 'iconfont icon-user',
+        103: 'iconfont icon-tijikongjian',
+        101: 'iconfont icon-shangpin',
+        102: 'iconfont icon-danju',
+        145: 'iconfont icon-baobiao'
+      },
+      /* 是否折叠 */
+      isCollapse: false,
+      /* 被激活的连接地址 */
+      activePath: ''
+    }
+  },
   methods: {
     logout() {
       // 清空token
       window.sessionStorage.clear()
       // 路由跳转到登录页面
       this.$router.push('/login')
+    },
+    /* 获取左侧菜单列表 */
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menuList = res.data
+    },
+    /* 点击按钮切换菜单的折叠与展开 */
+    toogleCollapse() {
+      this.isCollapse = !this.isCollapse
+    },
+    /* 保存连接的激活状态 */
+    saveNavState(activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+      /* 保存连接状态之后再同步data中立即更新界面 */
+      this.activePath = activePath
     }
   }
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 0;
+  div {
+    display: flex;
+    align-items: center;
+    font-size: 20px;
+    color: #fff;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    border-right: none;
+  }
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.iconfont {
+  margin-right: 10px;
+}
+.toogle-button {
+  background-color: #4a5064;
+  text-align: center;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  letter-spacing: 0.2em;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,27 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome'
+import Users from '../components/user/Users'
 
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: './login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home }
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users }
+    ]
+  }
 ]
 const router = new VueRouter({
   routes
